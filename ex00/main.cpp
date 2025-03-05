eb1a1ebb7ccaa81b90518c647f5334235be9316a
Leo Eemil Laaksonen <leo.laaksone@hotmail.com>
2025-03-05 19:35:24 +0200

    ex00 ready

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,13 +2,12 @@
 #include <cctype> // ??
 
 int main(int argc, char **argv) {
-	int i = 0;
 	if (argc == 1)
         	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";//character out / statement
 	if (argc > 1) {
-		while (argv[1][i] != '\0') {
-			std::cout << (char)toupper(argv[1][i]);
-			i++;
+		for(int j = 1; j < argc; j++) {
+			for (int i = 0; argv[j][i] != '\0'; i++)
+				std::cout << (char)toupper(argv[j][i]);
 		}
 		std::cout << "\n";
 	}
